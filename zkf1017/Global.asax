bdcf2adb40b0c565f06ad57696f1f2f8c9355258
stu <stu@qq.com>
2016-10-17 18:06:18 +0800

    添加项目文件。

---
+++ zkf1017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="zkf1017.MvcApplication" Language="C#" %>
---
+++ zkf1017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(zkf1017.Startup))]
+namespace zkf1017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
